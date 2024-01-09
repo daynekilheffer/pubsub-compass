006aef4a7d49495bd67f64f6da12fd251369687a
Dayne <dayne@scoir.com>
2024-01-09 10:43:15 -0500

    introduce monaco editor instead of text field

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import { Message, PubSub, Subscription } from '@google-cloud/pubsub';
-import { BrowserWindow, Menu, app } from 'electron';
-import { createIpcMain } from 'electron-typescript-ipc';
-import path from 'node:path';
-import { Api, TopicHierarchy } from './ipc-api';
+import { Message, PubSub, Subscription } from '@google-cloud/pubsub'
+import { BrowserWindow, Menu, app } from 'electron'
+import { createIpcMain } from 'electron-typescript-ipc'
+import path from 'node:path'
+import { Api, TopicHierarchy } from './ipc-api'
 
-import storage from 'electron-json-storage';
-import { applicationMenu } from './app-menu';
+import storage from 'electron-json-storage'
+import { applicationMenu } from './app-menu'
 
-const ipcMain = createIpcMain<Api>();
+const ipcMain = createIpcMain<Api>()
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 // eslint-disable-next-line global-require
@@ -23,6 +23,7 @@ const createWindow = () => {
     title: 'Pub/Sub Compass',
     webPreferences: {
       nodeIntegration: true,
+      nodeIntegrationInWorker: true,
       preload: path.join(__dirname, 'preload.js'),
     },
   })
@@ -30,9 +31,9 @@ const createWindow = () => {
 
   // and load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL)
   } else {
-    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`))
   }
 
   // Open the DevTools.
@@ -60,31 +61,29 @@ app.on('activate', () => {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
 
-
 app.whenReady().then(() => {
   const mainWindow = createWindow()
 
   process.env.PUBSUB_EMULATOR_HOST = 'localhost:8283'
 
   const pubsub = new PubSub({ projectId: 'local-psemu' })
-  const subs: Record<string, { inst: Subscription, handler: (msg: Message) => void }> = {}
+  const subs: Record<string, { inst: Subscription; handler: (msg: Message) => void }> = {}
 
   ipcMain.handle('listTopics', async () => {
-    const data = await pubsub.getTopics();
-    const topics = data[0];
+    const data = await pubsub.getTopics()
+    const topics = data[0]
     return Promise.all<Promise<TopicHierarchy>>(
       topics.map<Promise<TopicHierarchy>>(async (t) => ({
         name: t.name.replace(/projects\/local-psemu\/topics\//, ''),
         subscriptions: await t.getSubscriptions().then((subscriptions) => {
-          const sub = subscriptions[0];
+          const sub = subscriptions[0]
           return sub.map((s) => ({
             id: s.name,
-            name: s.name.replace(
-              /projects\/local-psemu\/subscriptions\//,
-              ''),
-          }));
+            name: s.name.replace(/projects\/local-psemu\/subscriptions\//, ''),
+          }))
         }),
-      })));
+      })),
+    )
   })
   ipcMain.handle('sendOnTopic', async (evt, name, payload, attrs = {}) => {
     const msgId = await pubsub.topic(name).publishMessage({
@@ -92,9 +91,7 @@ app.whenReady().then(() => {
       attributes: attrs,
     })
     console.debug('published', msgId)
-  }
-  )
-
+  })
 
   ipcMain.handle('resetSubscriptions', () => {
     console.log('resetting subscriptions')
